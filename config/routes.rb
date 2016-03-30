949332d1555af232f050c521a67de76f9e98b7de
Adrien Couture <coutureadrien@gmail.com>
2016-03-29 21:47:00 -0400

    Add helper methods to Application Controller, and restrict actions from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   resources :articles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
